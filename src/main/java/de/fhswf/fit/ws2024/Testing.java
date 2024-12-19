1bb9783935ebf4a5ddcc768bd7ac58b66bd63d7e
Dominik Schwabe <dominik.s01@gmx.de>
2024-12-20 00:15:53 +0100

    Test Klasse um einzelne Funktionen schnell zu testen

---
+++ src/main/java/de/fhswf/fit/ws2024/Testing.java
@@ -0,0 +1,19 @@
+package de.fhswf.fit.ws2024;
+
+import de.fhswf.fit.ws2024.beans.UserManager;
+import org.apache.commons.codec.digest.Crypt;
+
+public class Testing {
+    public static void main(String[] args) {
+        String generatedHash = Crypt.crypt("ABC");
+        System.out.println("generatedHash: " + generatedHash);
+
+        UserManager userManager = new UserManager();
+        userManager.setUsername("TestUser");
+        userManager.setPassword("TestPassword");
+        System.out.println("Username: " + userManager.getUsername());
+        System.out.println("Password: " + userManager.getPassword());
+        userManager.createUser();
+        System.out.println(userManager.login());
+    }
+}
